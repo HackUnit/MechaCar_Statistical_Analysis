95a20807ceb38c4942fabdbd54bed2a757ba1d78
HackUnit <j.larose26@gmail.com>
2022-09-25 17:53:48 -0500

    Added Resources dir, updated README, Rscript, gitignore

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,1 +1,15 @@
-read.csv(MechaCar_mpg)
+library(dplyr)
+MechaCar_mpg_df <- read.csv(file='Resources/MechaCar_mpg.csv',check.names=FALSE,stringsAsFactors = FALSE)
+
+
+lm(mpg ~ vehicle_length + 
+     vehicle_weight + 
+     spoiler_angle + 
+     ground_clearance + 
+     AWD,data=MechaCar_mpg_df)
+
+summary(lm(mpg ~ vehicle_length + 
+             vehicle_weight + 
+             spoiler_angle + 
+             ground_clearance + 
+             AWD,data=MechaCar_mpg_df))
